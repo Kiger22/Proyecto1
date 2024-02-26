3119e93739be1c943d8fd77e8a99afcd31169cc4
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-02-26 21:31:18 +0100

    Create java.js

---
+++ java.js
@@ -0,0 +1,31 @@
+console.log("hola")
+if (condition) {
+
+}
+
+switch (key) {
+  case value:
+
+    break;
+
+  default:
+    break;
+}
+
+while () {
+
+}
+
+do {
+
+} while (condition);
+
+for (let first of second) { third }
+
+console.log()
+
+function name(params) {
+
+}
+
+const
